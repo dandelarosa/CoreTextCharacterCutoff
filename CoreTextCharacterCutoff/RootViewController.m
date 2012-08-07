4070ad88635bd6cf532358a2e285a483ebbb7f9d
Dan Dela Rosa <dandelarosa@gmail.com>
2012-08-07 10:27:50 -0400

    example that doesn't render correctly

--- CoreTextCharacterCutoff/RootViewController.m
+++ CoreTextCharacterCutoff/RootViewController.m
@@ -12,6 +12,7 @@
 #import <CoreText/CTFramesetter.h>
 #import <CoreText/CTParagraphStyle.h>
 #import <CoreText/CTStringAttributes.h>
+#import <Foundation/NSDictionary.h>
 
 // Comment this out to hide guidelines
 #define SHOW_GUIDLINES 1
@@ -37,14 +38,14 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
-    CGRect viewRect = CGRectMake(50.0f, 50.0f, 345.0f, 120.0f);
+    CGRect viewRect = CGRectMake(50.0f, 50.0f, 92.0f, 95.0f);
     
     CATextLayer *textLayer = [CATextLayer layer];
     textLayer.contentsScale = [[UIScreen mainScreen] scale];
     textLayer.wrapped = YES;
     textLayer.truncationMode = kCATruncationNone;
-    UIFont *font = [UIFont fontWithName:@"SnellRoundhand" size:20.0f];
-    textLayer.string = @"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.";
+    UIFont *font = [UIFont fontWithName:@"AmericanTypewriter" size:22.0f];
+    textLayer.string = @"\n aaaa \n";
     textLayer.alignmentMode = kCAAlignmentLeft;
     textLayer.fontSize = font.pointSize;
     CTFontRef fontRef = CTFontCreateWithName((__bridge CFStringRef)font.fontName, font.pointSize, nil);
@@ -86,29 +87,29 @@
     
     UIImage *textImage2 = [self imageFromCATextLayer:textLayer andPaddingSize:CGSizeMake(10.0f, 10.0f)];
     UIImageView *textImageView2 = [[UIImageView alloc] initWithImage:textImage2];
-    textImageView2.frame = CGRectMake(40.0f, 340.0f, 365.0f, 140.0f);
+    textImageView2.frame = CGRectMake(40.0f, 340.0f, 110.0f, 114.0f);
     [self.view addSubview:textImageView2];
     UIImageView *textImageView3 = [[UIImageView alloc] initWithImage:textImage2];
-    textImageView3.frame = CGRectMake(440.0f, 40.0f, 365.0f, 140.0f);
+    textImageView3.frame = CGRectMake(440.0f, 40.0f, 110.0f, 114.0f);
     [self.view addSubview:textImageView3];
     
 #ifdef SHOW_GUIDLINES
     // Use these guidelines to test alignment
     UIColor *guidelineColor = [UIColor colorWithRed:0.0f green:1.0f blue:0.0f alpha:0.5f];
     
-    UIView *topGuideline = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 44.0f, 768.0f, 10.0f)];
+    UIView *topGuideline = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 70.0f, 768.0f, 10.0f)];
     topGuideline.backgroundColor = guidelineColor;
     [self.view addSubview:topGuideline];
     
-    UIView *leftGuideline = [[UIView alloc] initWithFrame:CGRectMake(40.0f, 0.0f, 10.0f, 1024.0f)];
+    UIView *leftGuideline = [[UIView alloc] initWithFrame:CGRectMake(46.0f, 0.0f, 10.0f, 1024.0f)];
     leftGuideline.backgroundColor = guidelineColor;
     [self.view addSubview:leftGuideline];
     
-    UIView *bottomGuideline = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 151.0f, 768.0f, 10.0f)];
+    UIView *bottomGuideline = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 92.0f, 768.0f, 10.0f)];
     bottomGuideline.backgroundColor = guidelineColor;
     [self.view addSubview:bottomGuideline];
     
-    UIView *rightGuideline = [[UIView alloc] initWithFrame:CGRectMake(395.0f, 0.0f, 10.0f, 1024.0f)];
+    UIView *rightGuideline = [[UIView alloc] initWithFrame:CGRectMake(105.0f, 0.0f, 10.0f, 1024.0f)];
     rightGuideline.backgroundColor = guidelineColor;
     [self.view addSubview:rightGuideline];
 #endif
@@ -157,9 +158,18 @@
     CTParagraphStyleSetting paragraphSettings = {kCTParagraphStyleSpecifierAlignment, sizeof(alignment), &alignment};
     CTParagraphStyleRef paragraphStyle = CTParagraphStyleCreate(&paragraphSettings, 1);
     
-    NSDictionary *attributes = [NSDictionary dictionaryWithObjectsAndKeys:layer.font, (NSString*)kCTFontAttributeName, paragraphStyle, kCTParagraphStyleAttributeName, layer.foregroundColor, kCTForegroundColorAttributeName, nil];
-    NSAttributedString *attrString = [[NSAttributedString alloc] initWithString:layer.string attributes:attributes];
-    CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString((__bridge_retained CFAttributedStringRef)attrString);
+    CFStringRef fontName = CTFontCopyFullName(layer.font);
+    CTFontRef fontRef = CTFontCreateWithName((CFStringRef)fontName, layer.fontSize, nil);
+    CFRelease(fontName);
+    
+    CFDictionaryValueCallBacks cbs = {0, NULL, NULL, NULL, NULL};
+    CFMutableDictionaryRef cfdict = CFDictionaryCreateMutable(NULL, 0, &kCFTypeDictionaryKeyCallBacks, &cbs);
+    CFDictionarySetValue(cfdict, kCTFontAttributeName, fontRef);
+    CFDictionarySetValue(cfdict, kCTParagraphStyleAttributeName, paragraphStyle);
+    CFDictionarySetValue(cfdict, kCTForegroundColorAttributeName, layer.foregroundColor);
+    NSMutableDictionary *dict = (__bridge NSMutableDictionary *)cfdict;
+    NSAttributedString *attrString = [[NSAttributedString alloc] initWithString:layer.string attributes:dict];
+    CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString((__bridge CFAttributedStringRef)attrString);
     CGMutablePathRef p = CGPathCreateMutable();
     CGPathAddRect(p, NULL, CGRectMake(10.0f, 2 * paddingHeight - 10.0f, textBounds.size.width, textBounds.size.height));
     CTFrameRef frame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0,0), p, NULL);
